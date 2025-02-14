479ee1ccfc9bbc5ada0fadea490c8d66a94ceb00
Erlend Svanevik <svme1124@gmail.com>
2025-02-14 13:26:18 +0100

    Add .gitignore file

--- obj/Debug/net9.0/CS-Basic-Oppgave-2-Customisert-God-Morgen-program.AssemblyInfo.cs
+++ obj/Debug/net9.0/CS-Basic-Oppgave-2-Customisert-God-Morgen-program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS-Basic-Oppgave-2-Customisert-God-Morgen-program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+528bfc106c14d1128e95eb87ac81d71fe193db7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS-Basic-Oppgave-2-Customisert-God-Morgen-program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS-Basic-Oppgave-2-Customisert-God-Morgen-program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
